ff5f508564e907b72f114158594bcc540a385b71
daniel-gomez-cano <daniel.gomez.cano@correounivalle.edu.co>
2024-05-04 18:58:14 -0500

    Primer commit, inicio del punto 1

---
+++ Punto1/src/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        FerreteriaGUI GUI = new FerreteriaGUI();
+        GUI.setVisible(true);
+    }
+}
